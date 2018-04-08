009065caeac1aceb8cbea89cb66be100d8cf1bb6
Himanshu Kankal <37235803+HimanUK@users.noreply.github.com>
2018-04-08 18:22:48 +0530

    Just contains the basic architecture.
    Plays 40 random moves.

---
+++ chess_basic.py
@@ -0,0 +1,23 @@
+'''
+Created on Sun Apr  8 18:14:14 2018
+@author: Himanshu Kankal
+'''
+
+import chess
+import random
+
+board = chess.Board()
+
+for i in range(40):
+    print(board)
+    
+    lgmov = list(board.legal_moves)
+    lgmov = [str(x) for x in lgmov]
+    
+    mov = random.choice(lgmov)
+    
+    now_move = chess.Move.from_uci(mov)
+    board.push(now_move)
+    
+    print('--------------------------------------- \n')
+    
